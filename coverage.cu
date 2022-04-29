f40f9d75a1e410b45d07a9381943d730d250600f
Thomas Lynn <32374143+lynntf@users.noreply.github.com>
2022-04-29 11:30:45 -0500

    circle boundary logic

--- coverage.cu
+++ coverage.cu
@@ -89,9 +89,6 @@ __global__ void rotate(int t, double* x, double* z, double* marker1,
         
         // Remove points that are outside the domain
         int outside = 0;
-        if (xl*xl + zl*zl > 1) {
-            outside = 1;
-        }
 
         ///////////////////////////////////////////////
         // Project from the flat grid to the hemisphere
@@ -100,11 +97,17 @@ __global__ void rotate(int t, double* x, double* z, double* marker1,
         switch (doprojection){
             case 1 :
                 // Stereographic projection
+                if (xl*xl + zl*zl > 1) {
+                    outside = 1;
+                }
                 xl = 2*xl/(1+ xl*xl+ zl*zl);
                 zl = 2*zl/(1+ temp*temp+ zl*zl);
                 break;
             case 2 :
                 // Lambert equal-area projection
+                if (xl*xl + zl*zl > 1) {
+                    outside = 1;
+                }
                 xl = sqrt(1.0 - (xl*xl + zl*zl) /2.0 )*xl*sqrt(2.0);
                 zl = sqrt(1.0 - (temp*temp + zl*zl) /2.0 )*zl*sqrt(2.0);
                 break;
@@ -118,6 +121,9 @@ __global__ void rotate(int t, double* x, double* z, double* marker1,
                 double lamb = atan2(xl*sin(c),(-zl*sin(c)));
                 xl = cos(phi_g)*cos(lamb);
                 zl = cos(phi_g)*sin(lamb);
+                if (xl*xl + zl*zl > 1) {
+                    outside = 1;
+                }
                 break;
             case 4 :
                 // Square to disk projection due to Shirely (1997)
